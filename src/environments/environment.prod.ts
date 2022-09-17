fadadc86711cf5de98c8f1b7dda1e46a5cbe5ed6
AnderSpidy <andersonreinaldo2347@gmail.com>
2022-09-16 21:16:03 -0300

    adicionando pasta envionment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB1XZHSdbpGNdopsd4vOZ5HgHhx9KIQZdc",
+    authDomain: "requisicoes-app-36e86.firebaseapp.com",
+    projectId: "requisicoes-app-36e86",
+    storageBucket: "requisicoes-app-36e86.appspot.com",
+    messagingSenderId: "819938975392",
+    appId: "1:819938975392:web:4e380eca7e166208fe33c5"
+  }
 };
